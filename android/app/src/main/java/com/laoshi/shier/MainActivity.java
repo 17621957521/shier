451b217fe148a0f71d36a757d59827383b1548c2
White <896745063@qq.com>
2023-04-24 14:02:23 +0800

    init

---
+++ android/app/src/main/java/com/laoshi/shier/MainActivity.java
@@ -0,0 +1,6 @@
+package com.laoshi.shier;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  //app主页
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+
+            // child: ,
+            ),
+      ),
+    );
+  }
+}
